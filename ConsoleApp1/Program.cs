0b85894d76e1509358a312086c8e6019e81e4a25
Mike Bucher <mikedjbucher@hotmail.com>
2021-12-01 11:44:15 +0000

    day 1 finished, time used, 15mins

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Day1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
